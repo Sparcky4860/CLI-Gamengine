96c720e42dc40d9034f063ff88cacf35e973742c
Sparcky <Lloydplays@iCloud.com>
2024-05-02 12:04:56 -0300

    ???

--- obj/Debug/net8.0/CLIGamengine.AssemblyInfo.cs
+++ obj/Debug/net8.0/CLIGamengine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CLIGamengine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c477603252456ec5204b81702cc88f838bcdefe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f876cf122691fbff088bb1149bd267b39d1f927")]
 [assembly: System.Reflection.AssemblyProductAttribute("CLIGamengine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CLIGamengine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
