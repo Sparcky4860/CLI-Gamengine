77e709395beb14945b053eaa8e1e2e08f4fa24b6
Pawos Howl <130191607+Pawos-Howl@users.noreply.github.com>
2024-06-10 11:10:02 -0700

    s

--- CLIGameBackup.cs
+++ CLIGameBackup.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 
 public class CLIGameBackup{
     private static CLIGameBackupData backup = new CLIGameBackupData();
+    private static string fileEnd = ".json"; // in case I want to change it later
     // https://stackoverflow.com/questions/18813475/c-sharp-saving-a-txt-file-to-the-project-root
     // private string backuploc = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"GAMEDATABACKUP.json");
     // private string backuploc = Path.Combine(Environment.CurrentDirectory,"GAMEDATABACKUP.json");
@@ -11,14 +12,14 @@ public class CLIGameBackup{
         backup.WriteCaches();
         if(!backup.ValidateCache()){return false;} // termination before serialization
         string gamedata = JsonSerializer.Serialize(backup);
-        File.WriteAllText(Path.Combine(Environment.CurrentDirectory,"GAMEDATABACKUP-"+name+".json"),gamedata);
+        File.WriteAllText(Path.Combine(Environment.CurrentDirectory,"saves/"+name+fileEnd),gamedata);
         return true; // everything should have run correctly
     }
 
     // returns false if there is an error
     // NOTE: IF IT RETURNS FALSE DO NOT USE THE BACKUP
     public static bool ReadBackup(string name){
-        string paf = Path.Combine(Environment.CurrentDirectory,"GAMEDATABACKUP-"+name+".json");
+        string paf = Path.Combine(Environment.CurrentDirectory,"saves/"+name+fileEnd);
         if(!File.Exists(paf)){return false;} // no backup file
         string filedata = File.ReadAllText(paf);
         CLIGameBackupData? temp;
@@ -34,5 +35,10 @@ public class CLIGameBackup{
         backup = temp;
         backup.UseBackup();
         return true; // this should be fine
+    }
+
+    // this is more used in another code segment
+    public static string getCurrentFileEnd(){
+        return fileEnd;
     }
 }
